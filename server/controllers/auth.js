b373f678ad54c801b53b04ff1794e0d64191392b
Simran Kaur <simran00kk@gmail.com>
2023-08-15 16:42:34 -0400

    adjusting middleware and values

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -6,8 +6,8 @@ auth.verifySignUp = (req, res, next) => {
     //values is like a variables array
     //first element and second element may be in req.body
     //note that you may want to hash password before storing in DB
-    const values = []
-    const verifySignUpQuery = `INSERT INTO Users (Username, Password)
+    const values = [req.body.username, req.body.password];
+    const verifySignUpQuery = `INSERT INTO users (username, password)
     VALUES ($1, $2)`
     db.query(verifySignUpQuery)
         .then(data =>{
@@ -22,27 +22,43 @@ auth.verifySignUp = (req, res, next) => {
           }))   
 }
 
-tableController.addTask = (req, res, next) =>{
-    
-    const values = [req.body.personid, req.body.completedStatus,req.body.item];
-    // const addTaskQuery = `INSERT INTO list VALUES (231, 'Incomplete', 'this works')`
-    const addTaskQuery = `INSERT INTO list VALUES ($1, $2, $3)`
-    // console.log(req.body) 
-       
-    db.query(addTaskQuery, values)
-        .then(data => {
-            console.log('afterquery')
-            console.log("data", data)
-            res.locals.newTaskRow = data.rows
-            return next()
+auth.verifyLogin = (req,res,next) => {
+    //search for a record in the users db with the given username and pw
+    const verifyLoginQuery = `SELECT * FROM users WHERE username = ${req.query.username} AND password = ${req.query.username}`;
+    db.query(verifyLoginQuery)
+        .then(data =>{
+            console.log('auth line 30 verify login data.rows', data.rows)
+            //do we want to send anything back besides a status 200?
+            return next();
         })
-        .catch((err) => next ({
-            log: 'Express error handler caught tableController.addTask error',
-            status: 400,
-            message: { err: 'An error occurred' },
+        .catch(err => next({
+            log: `Error ${err} at auth verifyLogin`,
+            status: 404,
+            message: 'Error: User Not Found'
           }))
-
 }
+
+//example from simran solo project
+// tableController.addTask = (req, res, next) =>{
+//     const values = [req.body.personid, req.body.completedStatus,req.body.item];
+//     // const addTaskQuery = `INSERT INTO list VALUES (231, 'Incomplete', 'this works')`
+//     const addTaskQuery = `INSERT INTO list VALUES ($1, $2, $3)`
+//     // console.log(req.body) 
+       
+//     db.query(addTaskQuery, values)
+//         .then(data => {
+//             console.log('afterquery')
+//             console.log("data", data)
+//             res.locals.newTaskRow = data.rows
+//             return next()
+//         })
+//         .catch((err) => next ({
+//             log: 'Express error handler caught tableController.addTask error',
+//             status: 400,
+//             message: { err: 'An error occurred' },
+//           }))
+
+// }
 
 
 module.exports = auth;
